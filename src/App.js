0b02e3945ed2fb60805136b68b552fea8182b7e6
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-01-03 14:11:32 +0700

    finishing interaction component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,44 @@ import { getRandomHexColor } from './utils';
 import './App.css';
 
 function App() {
-  const [color, setColor] = useState(getRandomHexColor());
+  const [color, setColor] = useState(() => {
+    let c;
+    try {
+      new Values(window.location.hash).all(10);
+      c = window.location.hash;
+    } catch (e) {
+      c = getRandomHexColor()
+    } finally {
+      return c;
+    }
+  });
   const [list, setList] = useState(new Values(color).all(10));
+  const [error, setError] = useState(false);
+
+  const submitColor = (e) => {
+    e.preventDefault();
+    const c = e.target.color.value;
+    onChangeColor(c)
+  }
+
+  const onChangeColor = (c) => {
+    try {
+      const colors = new Values(c).all(10);
+      setColor(c);
+      setList(colors);
+      setError(false)
+      window.location.hash = c
+    } catch (e) {
+      setError(true)
+      return
+    }
+  }
 
   return (
     <>
-      <Navbar />
+      <Navbar color={color} error={error} onChange={onChangeColor} onSubmit={submitColor} />
       <main className="colors">
-        {list.map(c => <Color color={c} />)}
+        {list.map(c => <Color key={c.hex} color={c} />)}
       </main>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar({ color, error, onChange, onSubmit }) {
     return (
         <nav className="navbar">
             <div className="nav-center">
                 <div className="nav-title">
-                    <label htmlFor="color"><h3>Color</h3></label>
+                    <label htmlFor="color">
+                        <h3>Color</h3>
+                    </label>
                 </div>
-                <form>
+                <form onSubmit={onSubmit}>
                     <input
+                        className={error ? 'error' : ''}
+                        defaultValue={color}
                         id="color"
                         name="color"
                         type='text'
                         placeholder='#fff135'
+                        onChange={(e) => onChange(e.target.value)}
                     />
                 </form>
             </div>
